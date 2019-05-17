581805b55e91ec3dbf8c6e0c40f9ee44f21b7f89
Beau August <beau.august@investoogroup.com>
2019-05-17 17:21:35 +0100

    Added body log

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const spengbab = str => str.split('').map(char => ((Math.random() < 0.5) ? char.
 express()
   .use(express.json())
   .post('/', (req, res) => {
+    console.log(req.body);
     if (!req.body.text || !req.body.text.length) {
       res.status(500).send({ response_type: 'ephemeral', text: 'Send a message, dummy' });
       return;
