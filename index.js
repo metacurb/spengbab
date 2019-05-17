a57849d2471875318ab835638225c268c2f6ae64
Beau August <beau.august@investoogroup.com>
2019-05-17 17:11:22 +0100

    Fixed and ready for heroku

--- index.js
+++ index.js
@@ -1,22 +1,18 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const PORT = process.env.PORT || 5000
 
 const spengbab = str => str.split('').map(char => ((Math.random() < 0.5) ? char.toUpperCase() : char)).join('');
 
-app.use(express.json());
+express()
+  .use(express.json())
+  .post('/', (req, res) => {
+    console.log(req);
+    if (!req.body.text || !req.body.text.length) {
+      res.status(500).send({ response_type: 'ephemeral', text: 'Send a message, dummy' });
+      return;
+    }
 
-app.post('/babify', (req, res) => {
-  console.log(req);
-  if (!req.body.text || !req.body.text.length) {
-    res.status(500).send({ response_type: 'ephemeral', text: 'Send a message, dummy' });
-    return;
-  }
-
-  res.send({ response_type: 'in_channel', text: spengbab(req.body.text) });
-});
-
-const server = app.listen(8080, (error) => {
-    if (error) return console.log(`Error: ${error}`);
- 
-    console.log(`Server listening on port ${server.address().port}`);
-});
+    res.send({ response_type: 'in_channel', text: spengbab(req.body.text) });
+  })
+  .get('/', (req, res) => res.send({ message: 'hey' }))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
