e6b8cb2b84a8745adbef65bfccd1fccc74be4ba9
Beau <beauagst@gmail.com>
2023-01-13 16:43:58 +0000

    commit

--- netlify/functions/spengbab.js
+++ netlify/functions/spengbab.js
@@ -1,4 +1,5 @@
 const querystring = require('node:querystring');
+const axios = require('axios');
 
 const spengbab = str => String(str)
   .trim()
@@ -12,7 +13,7 @@ const spengbab = str => String(str)
   }, { previousUpper: false, word: '' }).word;
 
 exports.handler = async function (event, context) {
-  const { text, user_id } = querystring.parse(event.body)
+  const { text, response_url, user_id } = querystring.parse(event.body)
 
   if (!text || !text.trim().length) {
     return {
@@ -21,32 +22,41 @@ exports.handler = async function (event, context) {
     }
   }
 
+  await axios.post(response_url, {
+    "text": "Oh hey, this is a fun message in a channel!",
+    "response_type": "in_channel"
+  })
+
   return {
-    statusCode: 200,
-		headers: {
-			'Content-type': 'application/json',
-		},
-    body: JSON.stringify({
-      response_type: 'in_channel',
-      blocks: [
-        {
-          type: 'section',
-          text: {
-            type: 'mrkdwn',
-            text: spengbab(text)
-          }
-        },
-        {
-          type: 'context',
-          elements: [
-            {
-              type: 'mrkdwn',
-              text: `- <@${user_id}>`
-            }
-          ]
-        }
-      ]
-    })
+    statusCode: 200
   }
+
+  // return {
+  //   statusCode: 200,
+	// 	headers: {
+	// 		'Content-type': 'application/json',
+	// 	},
+  //   body: JSON.stringify({
+  //     response_type: 'in_channel',
+  //     blocks: [
+  //       {
+  //         type: 'section',
+  //         text: {
+  //           type: 'mrkdwn',
+  //           text: spengbab(text)
+  //         }
+  //       },
+  //       {
+  //         type: 'context',
+  //         elements: [
+  //           {
+  //             type: 'mrkdwn',
+  //             text: `- <@${user_id}>`
+  //           }
+  //         ]
+  //       }
+  //     ]
+  //   })
+  // }
 };
 
