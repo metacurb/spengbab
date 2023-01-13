22f707120bc7920355e2b39c6e26cbe1de84d587
Beau <beauagst@gmail.com>
2023-01-13 15:40:53 +0000

    commit

--- netlify/functions/spengbab.js
+++ netlify/functions/spengbab.js
@@ -1,60 +1,4 @@
-// const functions = require('firebase-functions');
-// const express = require('express')
-
-// let previousUpper = false;
-
-// const spengbab = str => String(str)
-//   .trim()
-//   .toLowerCase()
-//   .split('')
-//   .map(char => {
-//     if (!/[a-zA-Z]/.test(char)) return char;
-//     const newChar = previousUpper ? char.toLowerCase() : char.toUpperCase();
-//     previousUpper = !previousUpper;
-//     return newChar;
-//   }).join('');
-
-// const app = express();
-
-// app.use(express.json());
-
-// app.post('/', (req, res) => {
-//   const { body: { text, user_id } } = req
-//   if (!text || !text.trim().length) {
-//     return res.status(200).send({
-//       text: spengbab('I know how to use spengbab\nSend a message, dummy'),
-//     });
-//   }
-
-//   return res.send({
-//     response_type: 'in_channel',
-//     blocks: [
-//       {
-//         type: 'section',
-//         text: {
-//           type: 'mrkdwn',
-//           text: spengbab(text)
-//         }
-//       },
-//       {
-//         type: 'context',
-//         elements: [
-//           {
-//             type: 'mrkdwn',
-//             text: `- <@${user_id}>`
-//           }
-//         ]
-//       }
-//     ]
-//   })
-// });
-
-// app.get('/', (req, res) => res.send({
-//   message: spengbab('What the hell do you want?')
-// }))
-
-
-// exports.spengbab = functions.https.onRequest(app);
+const querystring = require('node:querystring');
 
 const spengbab = str => String(str)
   .trim()
@@ -67,17 +11,43 @@ const spengbab = str => String(str)
     return newChar;
   }).join('');
 
-// Initializes your app with your bot token and signing 
+
 exports.handler = async function (event, context) {
-  // your server-side functionality
-  console.log(event)
+  const { text, user_id } = querystring.parse(event.body)
+
+  if (!text || !text.trim().length) {
+    return {
+      statusCode: 200,
+      body: spengbab('I know how to use spengbab\nSend a message, dummy'),
+    }
+  }
 
   return {
     statusCode: 200,
     headers: {
       'Content-Type': 'application/json'
     },
-    body: event.body
+    body: {
+      response_type: 'ephemeral',
+      blocks: [
+        {
+          type: 'section',
+          text: {
+            type: 'mrkdwn',
+            text: spengbab(text)
+          }
+        },
+        {
+          type: 'context',
+          elements: [
+            {
+              type: 'mrkdwn',
+              text: `- <@${user_id}>`
+            }
+          ]
+        }
+      ]
+    }
   };
 };
 
